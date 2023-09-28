dbdb9d8c7aad2fd4fe4d174aea43ba0978be9fc6
Michael Conti <michaelconti@uri.edu>
2023-09-28 12:02:42 -0400

    lec code

---
+++ code/50even.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Write a single for loop to print the first 50 even numbers
+
+int main(){
+
+        for(int i = 2; i <= 100; i+=2){
+            std::cout << i << " ";
+        }
+
+    return 0;
+}
